d51662023a7a4cca78c4f61935b2005cc99e2480
Yuragranovsky <yuragranovsky@gmail.com>
2024-03-21 22:19:31 +0200

    CW #9 Performance Test OOD

---
+++ src/telran/performance/strings/StringsJoinPerformance.java
@@ -0,0 +1,24 @@
+package telran.performance.strings;
+
+import telran.performance.PerformanceTest;
+import telran.strings.StringsJoin;
+
+public class StringsJoinPerformance extends PerformanceTest {
+    String[] array;
+    StringsJoin stringsJoin;
+	public StringsJoinPerformance(String displayName, int nRuns,
+			StringsJoin stringsJoin, String[] array) {
+		super(displayName, nRuns);
+		this.stringsJoin = stringsJoin;
+		this.array = array;
+		
+	}
+	@Override
+	protected void runTest() {
+		stringsJoin.joinStrings(array, ";");
+		
+	}
+    
+	
+
+}
